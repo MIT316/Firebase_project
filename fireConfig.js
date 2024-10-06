98244d7cfb394b65f739c52be122e8b08af5cb97
mithilesh upadhyay <106898686+MIT316@users.noreply.github.com>
2024-10-06 18:45:47 +0530

    Create fireConfig.js

---
+++ fireConfig.js
@@ -0,0 +1,16 @@
+// Your Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCtbUHwK00WoEA24qEI6A2ZVwtDYHm4qIs",
+    authDomain: "safetydevice-ad684.firebaseapp.com",
+    databaseURL: "https://safetydevice-ad684-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "safetydevice-ad684",
+    storageBucket: "safetydevice-ad684.appspot.com",
+    messagingSenderId: "385515071384",
+    appId: "1:385515071384:web:1a57caea68a7f0824aaded"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
